640bf9c9858567e026c3643802e3a3018a5dd42b
Tejas Pancholi <tejaser@users.noreply.github.com>
2015-09-27 17:25:14 +0530

    Create character_escaping.py

---
+++ character_escaping.py
@@ -0,0 +1,16 @@
+###Question
+dont_worry = "Don't worry about apostrophes"
+print(dont_worry)
+print("The name of this ice-cream is \"Sweeet\"")
+print('text')
+
+###Problem
+#Even if you write correct response it will still mark it as wrong due to pycharm wanting you to write the answer in a 
+#specific manner.
+
+###solution
+##Step 1:-
+#using tip(click on the light bulb icon) and click on convert single-quoted string to double quoted string.
+##Step 2:-
+#Now write your answer in the text field as below to get thru. Cheers!!
+print("The name of this ice-cream is \"Sweeet'n'Tasty\"")
